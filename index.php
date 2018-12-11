d08a3e1fe1c815c214f4aaa59f9a7634e84474e0
Lucas Souza <lsdo2011@gmail.com>
2018-12-11 18:58:44 -0200

    commit 3

--- index.php
+++ index.php
@@ -2,16 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql();
+	$page = new Page();
 
-	$res = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($res);
+	$page->setTpl("index");
 
 
 });
